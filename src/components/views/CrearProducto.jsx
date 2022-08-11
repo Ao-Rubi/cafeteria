ca74ded2f13dbc2638c67ad25f72a22e5b243848
Garcia Mauro Nicolas <pepe-chan1@hotmail.com>
2022-08-11 11:39:54 -0300

    Agregado routers para las paginas y maquetado del navbar

---
+++ src/components/views/CrearProducto.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CrearProducto = () => {
+    return (
+        <div>
+            <p>desde crear producto</p>
+        </div>
+    );
+};
+
+export default CrearProducto;
---
+++ src/components/views/EditarProductos.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EditarProductos = () => {
+    return (
+        <div>
+            <h1>Editar productos</h1>
+        </div>
+    );
+};
+
+export default EditarProductos;
