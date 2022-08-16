7c106f9864cc3c622b16a66ef5f866038fa19cc4
Garcia Mauro Nicolas <pepe-chan1@hotmail.com>
2022-08-16 11:20:44 -0300

    se agrego el maquetado de la pagina para administrar los productos

--- src/components/views/AdministrarProductos.jsx
+++ src/components/views/AdministrarProductos.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import Table from 'react-bootstrap/Table';
+import ItemProducto from './ItemProducto';
 
 const AdministrarProductos = () => {
     return (
-        <div>
-            <h1>Pagina para administrar productos</h1>
-        </div>
+        <section className='container'>
+            <div className='d-flex justify-content-between align-items-center mt-5'>
+                <h1 className='display-4'>Productos disponibles</h1>
+                <Button>Agregar</Button>
+            </div>
+
+            <hr />
+            <Table striped bordered hover responsive>
+                <thead>
+                    <tr>
+                        <th>cod</th>
+                        <th>producto</th>
+                        <th>precio</th>
+                        <th>imagen URL</th>
+                        <th>Categoria</th>
+                        <th>Opciones</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <ItemProducto></ItemProducto>
+                </tbody>
+
+            </Table>
+        </section>
     );
 };
 
---
+++ src/components/views/ItemProducto.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+const ItemProducto = () => {
+    return (
+        <div>
+            <tr>
+                <td>1</td>
+                <td>cafe</td>
+                <td>400</td>
+                <td>https://images.pexels.com/photos/4109850/pexels-photo-4109850.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1</td>
+                <td></td>
+                <td>
+                    <Button variant='warning'>Editar</Button>
+                    <Button variant='danger'>Borrar</Button>
+                </td>
+            </tr>
+        </div>
+    );
+};
+
+export default ItemProducto;
