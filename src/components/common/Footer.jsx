b45f0630fdd9bdeaaa20865ffa71f8c3c5221b07
Garcia Mauro Nicolas <pepe-chan1@hotmail.com>
2022-08-21 20:49:59 -0300

    footer abajo de la pagina

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
+        <div  className="position-fixed bottom-0">
             <p>Esto es el footer</p>
         </div>
     );
