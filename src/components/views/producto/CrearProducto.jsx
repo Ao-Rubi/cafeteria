8c5279cc2cdda2765c5039d2ac2b0775f54c4b25
Garcia Mauro Nicolas <pepe-chan1@hotmail.com>
2022-08-21 20:50:21 -0300

    agregado validaciones

--- src/components/views/producto/CrearProducto.jsx
+++ src/components/views/producto/CrearProducto.jsx
@@ -1,36 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { cantidadCaracteres, validarPrecio, validarImagen } from './helpers';
+
 
 const CrearProducto = () => {
+
+    const [nombreProducto, setNombreProducto] = useState("");
+    const [precio, setPrecio] = useState(0);
+    const [imagen, setImagen] = useState("");
+    const [categoria, setCategoria] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // Validar los datos
+        if (cantidadCaracteres(nombreProducto) && validarPrecio(precio) && validarImagen(imagen)) {
+            console.log("los datos son correctos crear el objero")
+        }else{
+            console.log("cargar bien los datos correctamente")
+        }
+
+        // crear un objeto
+
+        // Enviar peticion a Json-Server (API) create
+    }
+
     return (
         <section className='container'>
             <h1 className='display-4 mt-5'>Nuevo producto</h1>
             <hr />
 
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="nombreProducto">
                     <Form.Label>Nombre de producto</Form.Label>
-                    <Form.Control type="text" placeholder="Ej: Tostadas" />
+                    <Form.Control onChange={(e)=>{setNombreProducto(e.target.value)}} type="text" placeholder="Ej: Tostadas" />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="precioProducto">
                     <Form.Label>Precio</Form.Label>
-                    <Form.Control type="text" placeholder="Ej: 400$" />
+                    <Form.Control onChange={(e)=>{setPrecio(e.target.value)}} type="number" placeholder="Ej: 400" />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="imagenProducto">
                     <Form.Label>Imagen (URL)</Form.Label>
-                    <Form.Control type="text" placeholder="URL" />
+                    <Form.Control onChange={(e)=>{setImagen(e.target.value)}} type="text" placeholder="URL" />
                 </Form.Group>
 
                 <Form.Group className="mb-3">
                     <Form.Label>Categoria</Form.Label>
-                    <Form.Select>
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
+                    <Form.Select onChange={(e)=>{setCategoria(e.target.value)}}>
+                    <option value="">Seleccione una opcion</option>
+                    <option value="bebida-caliente" >Bebidas Calientes</option>
+                    <option value="bebida-fria">Bebidas Frias</option>
+                    <option value="dulce">Dulce</option>
+                    <option value="salado">Salado</option>
                     </Form.Select>
                 </Form.Group>
 
---
+++ src/components/views/producto/helpers.js
@@ -0,0 +1,29 @@
+export function cantidadCaracteres(input) {
+    
+    if (input.lenght >= 2 && input.lenght<= 20) {
+
+        return true; //Dato correcto
+    }else{
+
+        return false;//Dato incorrecto
+    }
+}
+
+export const validarPrecio = (dato) => {
+    let patron = /^[0-9]{1,4}$/
+    if (patron.test(dato)) {
+        return true
+    }else{
+        return false
+    }
+}
+
+export const validarImagen = (url) => {
+    let patron = /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})).?)(?::\d{2,5})?(?:[/?#]\S*)?$/;
+
+    if (patron.test(url)) {
+        return true
+    }else{
+        return false
+    }
+}
