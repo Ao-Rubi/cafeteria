88ee9adfa66c418bb6d45d896d0825973ce346de
Garcia Mauro Nicolas <pepe-chan1@hotmail.com>
2022-08-23 11:40:00 -0300

    actualizacion de api para borrar el producto

--- src/components/views/producto/ItemProducto.jsx
+++ src/components/views/producto/ItemProducto.jsx
@@ -1,9 +1,50 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import './producto.css'
+import Swal from 'sweetalert2';
 
 const ItemProducto = ({producto}) => {
     const {nombreProducto, id, categoria, imagen, precio} = {...producto}
+    const URL = process.env.REACT_APP_API_SERVER;
+
+    const handleDelete = ()=> {
+        Swal.fire({
+            title: 'Seguro de borrar?',
+            text: "No hay vuelta atras",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, Borrar!',
+            cancelButtonText: "Cancelar",
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                // Realizar la peticion para eliminar producto. DELETE
+
+                try {
+                    const parametro = {
+                        method: "DELETE"
+                    }
+                    const respuesta = await fetch(URL + "/" + id, parametro);
+                    console.log(respuesta)
+
+                    if (respuesta.status === 200) {
+                        
+                        Swal.fire(
+                            'Producto eliminado!',
+                            'Producto borrado correctamente.',
+                            'success'
+                        )
+                        // Recargar la tabla de productos
+                    }
+
+                } catch (error) {
+                    console.log(error)
+                    //Mostrar mensaje de error al usuario
+                }
+            }
+        })
+    }
 
     return (
             <tr>
@@ -14,7 +55,7 @@ const ItemProducto = ({producto}) => {
                 <td>{categoria}</td>
                 <td>
                     <Button variant='warning'>Editar</Button>
-                    <Button variant='danger'>Borrar</Button>
+                    <Button variant='danger' onClick={handleDelete}>Borrar</Button>
                 </td>
             </tr>
     );
