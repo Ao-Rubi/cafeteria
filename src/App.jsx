06acf3764ede5f8a995ac36758a2d6b6a997ab21
Garcia Mauro Nicolas <pepe-chan1@hotmail.com>
2022-08-28 22:14:17 -0300

    añadido la ruta de la pagina de detalles y añadido el componente mismo

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import EditarProductos from "./components/views/producto/EditarProductos"
 import Error404 from './components/views/Error404';
 import Menu from "./components/common/Menu";
 import Footer from "./components/common/Footer";
+import DetalleProductos from './components/views/producto/DetalleProductos';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route exact path='/administrar' element={<AdministrarProductos></AdministrarProductos>}></Route>
           <Route exact path='/administrar/crear' element={<CrearProducto></CrearProducto>}></Route>
           <Route exact path='/administrar/editar/:id' element={<EditarProductos></EditarProductos>}></Route>
+          <Route exact path="/detalles/:id" element={<DetalleProductos></DetalleProductos>}></Route>
 
           <Route path="*" element={<Error404></Error404>}></Route>
         </Routes>
---
+++ src/components/views/producto/DetalleProductos.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DetalleProductos = () => {
+    return (
+        <div>
+            Pagina de detalles
+        </div>
+    );
+};
+
+export default DetalleProductos;
